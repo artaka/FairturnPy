0c45f0c3f6543b2864aacfedbfaf9f54b82295b9
artaka <fefsert@gmail.com>
2025-11-01 01:10:44 +0300

    Added manual input & more exception processed
    Took 56 seconds

--- app.py
+++ app.py
@@ -1,91 +1,79 @@
-﻿import shutil
-import os
-from time import sleep
+﻿from time import sleep
 
 from ranging import processCsv
 from parserIN import parserIn
 from parserOUT import parserOut
+from manualIn import manualIn
 from check_token import check_token
-
-def center_text(text, width=None):
-    if width is None:
-        width = shutil.get_terminal_size().columns
-    return text.center(width)
-
-def clear_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def show_menu(title="FAIRTURN", options=None):
-
-    clear_console()
-    width = shutil.get_terminal_size().columns
-
-    # Верхняя рамка
-    print("═" * width)
-
-    # Заголовок
-    print(center_text(title, width))
-    print("─" * width)
-
-    # Опции меню
-    for option in options:
-        print(center_text(option, width))
-
-    # Нижняя рамка
-    print("═" * width)
-    print()
+from cli import *
 
 
 if __name__ == "__main__":
-    while True:
-        show_menu(
-            title="FAIRTURN",
-            options=[
-                "1. Process table from Google Sheets",
-                "2. Check token",
-                "0. Exit"
-            ]
-        )
+    try:
+        while True:
+            show_menu(
+                title="FAIRTURN",
+                options=[
+                    "1. Process table from Google Sheets",
+                    "2. Manual input",
+                    "3. Check token",
+                    "0. Exit"
+                ]
+            )
 
-        choice = input(center_text("Your choice: ")).strip()
+            choice = input(center_text("Your choice: ")).strip()
 
-        match choice:
-            case "1":
-                try:
-                    clear_console()
-                    parserIn()
-                    sleep(1)
-                    clear_console()
+            match choice:
+                case "1":
                     try:
-                        currentLab = int(input("Enter current lab number: "))
-                    except Exception as ex:
-                        print("Invalid number!")
+                        clear_console()
+                        parserIn()
+                        sleep(1)
+                        clear_console()
+                        try:
+                            currentLab = int(input("Enter current lab number: "))
+                        except Exception as ex:
+                            print("Invalid number!")
+                            sleep(2)
+                            continue
+
+                        processCsv(currentLab)
+                        clear_console()
+                        parserOut()
                         sleep(2)
-                        continue
+                    except Exception as ex:
+                        print(f"\033[31mError: {ex}\033[0m")
+                        sleep(5)
 
-                    processCsv(currentLab)
+                case "2":
+                    try:
+                        clear_console()
+                        manualIn()
+                        print("\nDouble press enter to continue")
+                        input()
+                        input()
+                    except Exception as ex:
+                        print(f"\033[31mError: {ex}\033[0m")
+                        sleep(5)
+
+                case "3":
+                    try:
+                        clear_console()
+                        check_token()
+                        sleep(2)
+                    except Exception as ex:
+                        print(f"\033[31mError: {ex}\033[0m")
+                        sleep(5)
+
+                case "0":
                     clear_console()
-                    parserOut()
-                    sleep(2)
-                except Exception as ex:
-                    print(f"\033[31mError: {ex}\033[0m")
-                    sleep(5)
+                    exit(0)
 
-            case "2":
-                try:
+                case _:
                     clear_console()
-                    check_token()
+                    print("\033[31mInvalid choice!\033[0m")
                     sleep(2)
-                except Exception as ex:
-                    print(f"\033[31mError: {ex}\033[0m")
-                    sleep(5)
-
-            case "0":
-                clear_console()
-                exit(0)
-
-            case _:
-                clear_console()
-                print("\033[31mInvalid choice!\033[0m")
-                sleep(2)
+    except KeyboardInterrupt:
+        clear_console()
+        print(center_text("Developed by artaka & m0loko"))
+        exit(0)
--- ranging.py
+++ ranging.py
@@ -1,47 +1,6 @@
 ﻿import csv
 import random
-
-class Human:
-    def __init__(self, name, prevLabs, submittingLabs, attended, current):
-        try:
-            self.name = name.capitalize().split(maxsplit=1)[0]
-            self.prevLabs = prevLabs
-            self.submittingLabs = int(submittingLabs)
-            self.attended = 0
-            if(attended.lower() == "да"):
-                self.attended = 10
-            self.current = current
-            self.total = None
-        except Exception as ex:
-            raise Exception("\033[31mError in Human.__init__(). Invalid data in table!\033[0m")
-
-    def getScore(self):
-        if(self.total != None):
-            raise Exception("\033[31mError in Human.getScore(). Total score is not empty!\033[0m")
-        try:
-            self.total = int(self.prevLabs) + int(self.submittingLabs) + self.attended
-        except Exception as ex:
-            raise Exception("\033[31mError in Human.getScore(). Error while getting total score: " + str(ex) + "\033[0m")
-
-        if(self.submittingLabs == self.current):
-            self.total -= 11
-
-        if(self.name == "Мышковец" or self.name == "Макаревич"):
-            self.total -= 2
-
-        return self.total
-
-    def getHumanCSVRow(self):
-        if(self.total == None):
-            raise Exception("\033[31mError in Human.getHumanCSVRow(). Total score is empty!\033[0m")
-        return [
-            self.name,
-            self.prevLabs,
-            self.submittingLabs,
-            "Да" if self.attended == 10 else "Нет",
-            self.current,
-            self.total
-        ]
+from human import Human
 
 
 def processCsv(currentLab):
